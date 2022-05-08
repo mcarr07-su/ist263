7c0d5a45b742f0724a30e2b1617ea72b0ecb6ee4
mcarr07-su <98543227+mcarr07-su@users.noreply.github.com>
2022-05-08 18:24:32 -0400

    project7

---
+++ project7/script.js
@@ -0,0 +1,15 @@
+var countDownDate = new Date("May 14, 2022 12:30:25").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "I graduated!";
+  }
+}, 1000);
